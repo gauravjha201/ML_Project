ed3cdc7d6b3003e32b4646b08ae5f178bca0c127
gauravjha201 <jhagaurav034@gmail.com>
2026-02-05 07:07:54 +0530

    py

--- src/exception.py
+++ src/exception.py
@@ -6,6 +6,7 @@ def error_message_detail(error,error_detail:sys):
     file_name=exe_tb.tb_frame.f_code.co_filename
     error_message="Error Occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exe_tb.tb_lineno,str(error)
+        
     )
 
 class CustomException(Exception):
