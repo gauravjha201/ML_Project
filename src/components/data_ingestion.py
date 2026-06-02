b8dc04e4acdc51aadaa2ff4175693745bd26d7fc
gauravjha201 <jhagaurav034@gmail.com>
2026-06-02 17:55:20 +0530

    py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -20,6 +20,7 @@ class DataIngestionConfig:
     test_data_path: str = os.path.join("artifacts", "test.csv")
     raw_data_path: str = os.path.join("artifacts", "data.csv")
 
+
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
